8d2ba440a9d73de0c76ccf0ba3f334a823e43b36
chensx <chensx@500wan.com>
2021-05-13 14:05:52 +0800

    aa

--- dataStruct/basicAlgo.go
+++ dataStruct/basicAlgo.go
@@ -10,18 +10,16 @@ func BinarySearch(data []int, target int) (int){
 	if len(data) < 0 {
 		return -1
 	}
-	right := len(data)
+	right := len(data)-1
 	left := 0
-	for i:=0; i<len(data);i++{
-		mid := (left+right)/2
+	for left <= right {
+		mid := (right+left)/2
 		if data[mid] == target {
 			return mid
-		}
-		if data[mid] > target{
-			right = mid
-		}
-		if data[mid] < target {
-			left = mid
+		}else if data[mid] > target {
+			right = mid-1
+		}else if data[mid] < target {
+			left = mid+1
 		}
 	}
 	return -1
