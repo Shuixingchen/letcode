764b4d8430f967c66605a5b14386eca75dce72e6
csx <120781991@qq.com>
2022-08-05 14:29:53 +0800

    modify tcp read

--- mytcp/client.go
+++ mytcp/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
 	"fmt"
 	"io"
@@ -15,8 +16,7 @@ import (
 func main() {
 	rows := ReadFile()
 	var wg sync.WaitGroup
-	param := os.Args[1]
-	num, _ := strconv.Atoi(param)
+	num, _ := strconv.Atoi(os.Args[1])
 	fmt.Println("tcp num :", num)
 	wg.Add(num)
 	for i := 0; i < num; i++ {
@@ -29,11 +29,41 @@ func start(name string, wg *sync.WaitGroup) {
 	defer wg.Done()
 	conn, err := net.Dial("tcp", "47.243.70.145:3333")
 	if err != nil {
-		fmt.Println("conne", err)
+		fmt.Println("Dial error", err)
 		return
 	}
 	fmt.Println("start " + name)
 	cConnHandler(conn, strings.Trim(name, " "))
+}
+
+func cConnHandler(c net.Conn, name string) {
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go ReadLoop(c, name, &wg)
+	go func() {
+		c.Write([]byte("{\"id\":1,\"method\":\"mining.subscribe\",\"params\":[\"bmminer/0.0\"]}\n"))
+		c.Write([]byte("{\"id\":2,\"method\":\"mining.authorize\",\"params\":[\"" + name + "\", \"\"]}\n"))
+		wg.Done()
+	}()
+	wg.Wait()
+	log.Println(name + " closed")
+	c.Close()
+}
+
+func ReadLoop(c net.Conn, name string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		r := bufio.NewReader(c)
+		line, _, err := r.ReadLine()
+		if err != nil {
+			if err != io.EOF {
+				log.Println(name+" read error:", err)
+			}
+			break
+		} else {
+			log.Printf(name+" read %d bytes, content is %s\n", string(line))
+		}
+	}
 }
 
 func ReadFile() [][]string {
@@ -49,33 +79,4 @@ func ReadFile() [][]string {
 	r.FieldsPerRecord = -1
 	rows, _ := r.ReadAll()
 	return rows
-}
-
-func cConnHandler(c net.Conn, name string) {
-	var wg sync.WaitGroup
-	wg.Add(2)
-	go func() {
-		for {
-			var buf = make([]byte, 1024)
-			n, err := c.Read(buf)
-			if err != nil {
-				if err != io.EOF {
-					log.Println(name+" read error:", err, n)
-				}
-				break
-			} else {
-				// log.Printf(name+" read %d bytes, content is %s\n", n, string(buf[:n]))
-			}
-		}
-		wg.Done()
-	}()
-	go func() {
-		c.Write([]byte("{\"id\":1,\"method\":\"mining.subscribe\",\"params\":[\"bmminer/0.0\"]}\n"))
-		// c.Write([]byte("{\"id\":2,\"method\":\"mining.authorize\",\"params\":[" + name + ", \"\"]}\n"))
-		c.Write([]byte("{\"id\":2,\"method\":\"mining.authorize\",\"params\":[\"" + name + "\", \"\"]}\n"))
-		wg.Done()
-	}()
-	wg.Wait()
-	log.Println(name + " closed")
-	c.Close()
 }
